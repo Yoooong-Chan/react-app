a68b10e02f967709c533aebffed8ee7a63786870
Chan <rhdlwmal1234@naver.com>
2020-08-25 17:57:43 +0900

    배열항목삭제하기
    filter 이용해서 배열항목삭제하기

--- src/ArraySample.js
+++ src/ArraySample.js
@@ -1,38 +1,23 @@
 import React from 'react';
 
-function User({user}) {
+function User({user, onRemove}) {
   // users.map의 value가 파라미터 user에 들어있음
+  // {} 있어야함
+  const {username, email, id} = user;
   return (
     <div>
       <b>{user.username}</b>
       <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
 }
 
-export default function ArraySample() {
-  const users = [
-    {
-      id: 1,
-      username: 'kimchan',
-      email: 'gogo@naver.com',
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'banana@naver.com',
-    },
-    {
-      id: 3,
-      username: 'zeus',
-      email: 'khna@naver.com',
-    },
-  ];
-
+export default function ArraySample({users, onRemove}) {
   return (
     <div>
       {users.map((user) => (
-        <User key={user.id} user={user} />
+        <User key={user.id} user={user} onRemove={onRemove} />
       ))}
       {/*  map ( user , index )  => users의 값들을 파라미터 user로 전달함  
       => 각 user마다 key값을 할당해줘야 성능면에서좋아짐 
